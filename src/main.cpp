4ccf7e67c0344e0c9a3ae65a6f60ec29ae978c25
stepan.rutz <stepan.rutz@gmx.de>
2026-01-06 11:26:57 +0100

    hi-dpi

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication::setStyle(QStyleFactory::create("Fusion"));
     QApplication a(argc, argv);
     MainWindow w;
